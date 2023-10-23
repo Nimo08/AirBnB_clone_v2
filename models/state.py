976d984852bf354328438fce54544324a4800bdd
Nimo08 <nimomohamed800@gmail.com>
2023-10-24 00:33:29 +0300

    Re-adding cities getter

--- models/state.py
+++ models/state.py
@@ -3,11 +3,27 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state",
-                          cascade="all, delete-orphan")
+
+    storage_type = os.getenv('HBNB_TYPE_STORAGE')
+    if storage_type == 'db':
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute"""
+            from models.city import City
+            from models import storage
+            # get all City instances
+            res = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    res.append(city)
+            return res
