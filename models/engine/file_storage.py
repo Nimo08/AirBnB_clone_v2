5e711201be7f800ff8f11881ce7c8669562ee80f
Nimo08 <nimomohamed800@gmail.com>
2023-10-23 12:32:46 +0300

    Editing all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            filtered_obj = {}
+            for key, value in FileStorage.__objects.items():
+                if (isinstance(value, cls)):
+                    filtered_obj[key] = value
+            return filtered_obj
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
