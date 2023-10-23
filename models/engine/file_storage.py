b70b0d690a58bded835b38c16f1ef05b429404fe
Nimo08 <nimomohamed800@gmail.com>
2023-10-23 12:37:54 +0300

    Added else clause

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,6 +62,7 @@ class FileStorage:
             return
         # to avoid changing the dict, make a copy of the keys
         # iterate over the copy instead
+    else:
         for key in list(FileStorage.__objects.keys()):
             if obj == FileStorage.__objects[key]:
                 del FileStorage.__objects[key]
