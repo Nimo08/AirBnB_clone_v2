b1858673e84c9a3a48aef1a861f94b1c3e8c6943
Nimo08 <nimomohamed800@gmail.com>
2023-10-24 21:18:51 +0300

    Reverting to task 6 files

--- models/city.py
+++ models/city.py
@@ -13,5 +13,3 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     state = relationship('State', back_populates='cities')
-    places = relationship('Place', back_populates='cities',
-                          cascade="all, delete-orphan")
