233176e0a7c3b8009e690c7075a8fce43f8ede83
Nimo08 <nimomohamed800@gmail.com>
2023-11-08 21:47:39 +0300

    removed sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
   </head>
@@ -11,7 +11,7 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-sudo ln -sfn  /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "56i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
+ln -sfn  /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "56i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+service nginx restart
