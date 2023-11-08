441698c8cad685973b73c2e6fa9d13695af454fe
Nimo08 <nimomohamed800@gmail.com>
2023-11-08 21:43:58 +0300

    Added tabs

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,5 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sfn  /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i ' 56i location /hbnb_static/ {\
-	        alias /data/web_static/current/;\
-	}' /etc/nginx/sites-available/default
+sudo sed -i "56i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
