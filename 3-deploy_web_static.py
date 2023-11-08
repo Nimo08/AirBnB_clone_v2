d33b8cd7f27331142f2ab0f953da78c762bbf190
Nimo08 <nimomohamed800@gmail.com>
2023-11-08 19:20:46 +0300

    changed return value of do_deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -56,4 +56,6 @@ def deploy():
     archive_path = do_pack()
     if not archive_path:
         return False
-    return do_deploy(archive_path)
+    if do_deploy(archive_path):
+        return True
+    return False
