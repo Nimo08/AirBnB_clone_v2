4c09e08a2a535592b263f21cb141c4dbe58d4c18
Nimo08 <nimomohamed800@gmail.com>
2023-11-24 14:16:08 +0300

    removed teardown from import

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ LI tag: description of one State: <state.id>: <B><state.name></B>
 """
 
 
-from flask import Flask, render_template, teardown_appcontext
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
