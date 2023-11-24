3577e716bead862272fbd33463c3c7f16c3ed72e
Nimo08 <nimomohamed800@gmail.com>
2023-11-24 17:56:43 +0300

    Removing sort in .py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -34,8 +34,7 @@ def states_list():
     sorted by name
     """
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
