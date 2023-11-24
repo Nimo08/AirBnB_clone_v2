ab2ca080ad2ef99fe455009e625462e7da5e5978
Nimo08 <nimomohamed800@gmail.com>
2023-11-24 16:16:21 +0300

    pycodestyle error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -27,6 +27,7 @@ def states_list():
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception=None):
     """
