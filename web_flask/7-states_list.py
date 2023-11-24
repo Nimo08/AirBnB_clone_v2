0db70648de299a4deecd6ebbed3cb178470f0775
Nimo08 <nimomohamed800@gmail.com>
2023-11-24 10:52:04 +0300

    Task 8 attempt

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage
+sorted by name (A->Z)
+LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception=None):
+    """
+    Remove current SQLAlchemy Session after each request
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    Display a HTML page with list of all state obj in dbstorage
+    sorted by name
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
