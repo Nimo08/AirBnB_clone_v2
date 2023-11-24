f3a7c81f32a36e6179812a11c72b15ae285c8cde
Nimo08 <nimomohamed800@gmail.com>
2023-11-24 17:44:17 +0300

    Sorting in .py file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -34,7 +34,8 @@ def states_list():
     sorted by name
     """
     states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == "__main__":
