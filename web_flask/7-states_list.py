11fb4a72b40e7dd9665a66d5a6881834aa7aa2f7
Nimo08 <nimomohamed800@gmail.com>
2023-11-24 14:28:49 +0300

    Placed teardown at bottom of script

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,14 +18,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    """
-    Remove current SQLAlchemy Session after each request
-    """
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """
@@ -34,6 +26,14 @@ def states_list():
     """
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    """
+    Remove current SQLAlchemy Session after each request
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
