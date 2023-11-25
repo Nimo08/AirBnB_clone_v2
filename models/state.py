5013a056197592cb393c8e8c2046e8fc31714c0d
Nimo08 <nimomohamed800@gmail.com>
2023-11-25 09:56:25 +0300

    reverted state class

--- models/state.py
+++ models/state.py
@@ -8,14 +8,15 @@ import os
 
 class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    storage_type = os.getenv('HBNB_TYPE_STORAGE')
     if storage_type == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
         cities = relationship('City', back_populates='state',
                               cascade="all, delete-orphan")
-    else:
-        name = ""
 
+    else:
         @property
         def cities(self):
             """Getter attribute"""
