a9557fb42519765812228146aee3002ffc62457e
Nimo08 <nimomohamed800@gmail.com>
2023-11-25 14:17:24 +0300

    reverted state class

--- models/state.py
+++ models/state.py
@@ -10,7 +10,6 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
     storage_type = os.getenv('HBNB_TYPE_STORAGE')
     if storage_type == 'db':
         cities = relationship('City', back_populates='state',
